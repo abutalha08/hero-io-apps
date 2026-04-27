7d86c3229423fc621126a9cf6353a6b88b8d3c02
abutalha08 <taufique752@gmail.com>
2026-04-28 02:23:58 +0600

    refactor: improve routing structure by handling scroll restoration in RootLayout

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,11 +1,19 @@
 
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import Navbar from "../components/shared/Navbar";
 import Footer from "../components/shared/Footer";
 import { ToastContainer} from 'react-toastify';
+import { useEffect } from "react";
 
 
 const RootLayout = () => {
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div>
 
